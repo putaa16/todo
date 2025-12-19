2c7d67619a3f4aa7de3457942ddcc965f7843714
putaa16 <aputraa30@gmail.com>
2025-12-19 13:19:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const taskInput = document.getElementById("task-input");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(taskInput.value === ''){
+        alert("Anda harus menulis sesuatu!")
+    }
+    else{
+        let li = document.createElement("li")
+        li.innerHTML = taskInput.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    taskInput.value = "";
+    saveData();
+}  
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("uchecked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false); 
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
